262a906f98bed7e258453db0435bd4396a5753c9
adityamhatre2510 <adityamhatre2510@gmail.com>
2025-02-24 10:21:20 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -44,7 +44,8 @@ function init() {
     loadImages();
     createSnake();
     locateApple();
-    setTimeout("gameCycle()", DELAY);
+    setTimeout(gameCycle, DELAY); 
+
 }    
 
 function loadImages() {
